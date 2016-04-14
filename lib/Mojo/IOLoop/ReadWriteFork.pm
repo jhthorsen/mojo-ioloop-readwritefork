cf16df3f81c9dbb6f5239aa69c121778b7ec4544
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-04-14 09:54:34 +0200

    Released version 0.18
     - Improved documentation
     - Add conduit() attribute
     - Remove deprecated reactor() attribute

--- lib/Mojo/IOLoop/ReadWriteFork.pm
+++ lib/Mojo/IOLoop/ReadWriteFork.pm
@@ -18,7 +18,7 @@ sub ESC {
   $_;
 }
 
-our $VERSION = '0.17';
+our $VERSION = '0.18';
 
 our @SAFE_SIG = grep {
   not /^(
@@ -276,7 +276,7 @@ Mojo::IOLoop::ReadWriteFork - Fork a process and read/write from it
 
 =head1 VERSION
 
-0.17
+0.18
 
 =head1 DESCRIPTION
 
