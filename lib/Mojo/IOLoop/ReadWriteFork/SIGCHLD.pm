e5c21defccade504246b40e0b9ee2b9c45b2bb8a
Jan Henning Thorsen <jhthorsen@cpan.org>
2021-03-19 13:27:40 +0900

    Forgot to test with EV and Test::Pod::Coverage

--- lib/Mojo/IOLoop/ReadWriteFork/SIGCHLD.pm
+++ lib/Mojo/IOLoop/ReadWriteFork/SIGCHLD.pm
@@ -24,7 +24,7 @@ sub waitpid {
   # See https://github.com/kraih/minion/issues/15 and
   # https://github.com/jhthorsen/mojo-ioloop-readwritefork/issues/9 for details.
   my $reactor = Mojo::IOLoop->singleton->reactor;
-  return EV::child($pid, 0, sub { $self->_exit($pid, $_->rstatus) })
+  return $self->{ev}{$pid} ||= EV::child($pid, 0, sub { $self->_exit($pid, shift->rstatus) })
     if !$SIG{CHLD} and $reactor->isa('Mojo::Reactor::EV');
 
   weaken $self;
@@ -44,7 +44,44 @@ sub waitpid {
 sub _exit {
   my ($self, $pid, $status) = @_;
   my $listeners = delete $self->{pids}{$pid};
+  delete $self->{ev}{$pid};
   for my $cb (@$listeners) { $cb->($status, $pid) }
 }
 
 1;
+
+=head1 NAME
+
+Mojo::IOLoop::ReadWriteFork::SIGCHLD - Non-blocking waitpid for Mojolicious
+
+=head1 DESCRIPTION
+
+L<Mojo::IOLoop::ReadWriteFork::SIGCHLD> is a module that can wait for a child
+process to exit. This is currently done either with L<EV/child> or a recurring
+timer and C<waitpid>.
+
+=head1 METHODS
+
+=head2 is_waiting
+
+  $bool = $sigchld->is_waiting;
+
+Returns true if C<$sigchld> is still waiting for a process to exit.
+
+=head2 singleton
+
+  $sigchld = Mojo::IOLoop::ReadWriteFork::SIGCHLD->singleton;
+
+Returns a shared L<Mojo::IOLoop::ReadWriteFork::SIGCHLD> object.
+
+=head2 waitpid
+
+  $sigchld->waitpid($pid, sub { my ($exit_value) = @_ });
+
+Will call the provided callback with C<$?> when the C<$pid> is no longer running.
+
+=head1 SEE ALSO
+
+L<Mojo::IOLoop::ReadWriteFork>.
+
+=cut
